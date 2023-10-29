cc099ed2143287b65333ee9dbb7f9fb4a63a3293
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2023-10-29 09:32:18 +0100

    image-optimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.pixabay.com',
+                port: '',
+                pathname: '/photo/**',
+            },
+        ],
+    },
+}
